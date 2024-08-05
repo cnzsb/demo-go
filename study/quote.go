905a6917b1a16961f4d9e47688f1f93922bc6f6b
Ryan.Zhao <cnzsb@foxmail.com>
2024-08-06 00:37:01 +0800

    mod init + tidy + run

---
+++ study/quote.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "rsc.io/quote"
+
+func main() {
+	fmt.Println(quote.Glass())
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Opt())
+}
